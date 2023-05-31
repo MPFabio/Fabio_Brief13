937fa55a977bfb267d7dc567a57f3df064fff6e7
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-31 10:56:54 +0200

    Update outputs.tf

--- prod-env/outputs.tf
+++ prod-env/outputs.tf
@@ -6,6 +6,18 @@ output "Environment" {
   value = module.ChildModule.Environment
 }
 
+output "The_subnet_ID" {
+ value = module.ChildModule.The_subnet_ID
+}
+
+output "The_vnet_ID" {
+ value = module.ChildModule.The_vnet_ID
+}
+
 output "The_webserver_Public_ip" {
    value = module.ChildModule.The_webserver_Public_ip
+}
+
+output "The_websrever_Private_ip" {
+   value = module.ChildModule.The_webserver_Private_ip
 }
