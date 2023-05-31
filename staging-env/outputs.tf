dcf64f1058d5a5475f3c2d3ff89325204a482007
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-31 11:42:30 +0200

    Update outputs.tf

--- staging-env/outputs.tf
+++ staging-env/outputs.tf
@@ -1,24 +1,24 @@
   GNU nano 6.0                                                                                outputs.tf                                                                                          
 output "Ressource_group_name" {
-  value = module.ParentdModule.Ressource_group_name
+  value = module.ChildModule.Ressource_group_name
 }
 
 output "Environment" {
-  value = module.ParentdModule.Environment
+  value = module.ChildModule.Environment
 }
 
 output "The_subnet_ID" {
- value = module.ParentdModule.The_subnet_ID
+ value = module.ChildModule.The_subnet_ID
 }
 
 output "The_vnet_ID" {
- value = module.ParentdModule.The_vnet_ID
+ value = module.ChildModule.The_vnet_ID
 }
 
 output "The_webserver_Public_ip" {
-   value = module.ParentdModule.The_webserver_Public_ip
+   value = module.ChildModule.The_webserver_Public_ip
 }
 
 output "The_webserver_Private_ip" {
-   value = module.ParentdModule.The_webserver_Private_ip
+   value = module.ChildModule.The_webserver_Private_ip
 }
