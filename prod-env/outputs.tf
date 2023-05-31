0ed26004d3b2e9b2b7a85cf3fe83305bff747b56
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-05-31 11:35:55 +0200

    Update outputs.tf

--- prod-env/outputs.tf
+++ prod-env/outputs.tf
@@ -1,23 +1,23 @@
 output "Ressource_group_name" {
-  value = module.ChildModule.Ressource_group_name
+  value = module.ParentdModule.Ressource_group_name
 }
 
 output "Environment" {
-  value = module.ChildModule.Environment
+  value = module.ParentdModule.Environment
 }
 
 output "The_subnet_ID" {
- value = module.ChildModule.The_subnet_ID
+ value = module.ParentdModule.The_subnet_ID
 }
 
 output "The_vnet_ID" {
- value = module.ChildModule.The_vnet_ID
+ value = module.ParentdModule.The_vnet_ID
 }
 
 output "The_webserver_Public_ip" {
-   value = module.ChildModule.The_webserver_Public_ip
+   value = module.ParentdModule.The_webserver_Public_ip
 }
 
 output "The_webserver_Private_ip" {
-   value = module.ChildModule.The_webserver_Private_ip
+   value = module.ParentdModule.The_webserver_Private_ip
 }
